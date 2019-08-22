0410395a600495c373d74144e552b865c7fe01de
Ryhan Sunny <SUNNYRAYHAN2@GMAIL.COM>
2019-08-22 22:13:58 +0200

    fetch movie rating and print training and testing data

---
+++ recommendation_system_ai.py
@@ -0,0 +1,24 @@
+import numpy as np
+from lightfm.datasets import fetch_movielens # movielens is a data set of 100k real movie ratings
+from lightfm import LightFM # lightfm performs recommendation algorithm
+
+# fetching data and formatting it
+data = fetch_movielens(min_rating = 4.0) # CSV file
+
+# the fetch_movielens method will create an INTERACTION MATRIX from
+# our CSV file and store it in our data variable as a DICTIONARY
+
+# print-training and testing
+print(repr(data['print']))
+print(repr(data['test']))
+
+# our fetch_movielens method splits our data set into 'training' and 'testing' data
+# and we can retrieve each by using the 'train' and 'test'strings as KEYS (dictionary keys)
+# we printed out both above
+
+# todo: install lightfm again, run the code, store our model in a variable
+
+
+
+
+
